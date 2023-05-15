f7dec6ee71cf7275a6bd960f4253de54ecedc05c
Shayan Kabir <shayan.kabir@gmail.com>
2023-05-15 19:42:51 +1000

    Completed the GUI (without the functionalities)

--- HSC_Assessment.py
+++ HSC_Assessment.py
@@ -8,7 +8,11 @@ global interval
 global txt
 interval = 0
 
-#Radiobuttons
+
+ayo = LabelFrame(root, text = "experiment", font =40)
+ayo.place(x=200,y=250)
+
+#Radiobuttons (Search)
 r= IntVar()
 r.set("1")
 Radiobutton1 = Radiobutton(root, text ="Linear", variable = r, value =1, font =20)
@@ -23,8 +27,35 @@ Radiobutton2.place(x=600,y=100)
 
 Radiobutton2.place_forget()
 
+#Radiobuttons (Sort)
+V = IntVar()
+V.set("1")
+
+Radiobutton3 = Radiobutton(root, text ="Selection", variable = r, value =1, font =20)
+
+Radiobutton3.place_forget()
+
+Radiobutton4 = Radiobutton(root, text ="Bubble", variable = r, value =2, font =20)
+
+Radiobutton4.place_forget()
+
+Radiobutton5 = Radiobutton(root, text ="Insertion", variable = r, value =3, font =20)
+Radiobutton5.place(x=500,y=100)
+
+Radiobutton5.place_forget()
+
+
+Ascending =  Radiobutton(root, text ="Ascending", variable = V, value =1, font =20)
+Ascending.place(x=197,y=280)
+Ascending.place_forget()
+
+Descending =  Radiobutton(root, text ="Descending", variable = V, value =2, font =20)
+Descending.place(x=197,y=330)
+Descending.place_forget()
+
+
 #Entry Widgets
-widget = Entry(root, font =40)
+widget = Entry(root, font =40, fg="dark blue")
 widget.place(x=400,y=200)
 
 widget.place_forget()
@@ -35,33 +66,61 @@ widget2.place(x=400,y=300)
 widget2.place_forget()
 
 #Labels
-label = Label(root, text='Menu', fg='black', bg='blanched almond', font = ('Aerial bold',40), width=26, height=1).place(x=0,y=0)
+menu_label = Label(root, text='Menu', fg='black', bg='blanched almond', font = ('Aerial bold',40), width=26, height=1).place(x=0,y=0)
 
 option = Label(root, text='Please select one', fg='black', bg='blanched almond', font = ('Aerial',25))
 option.place(x=265,y=220)
 
+search_label = Label(root, text='Searches', fg='black', bg='blanched almond', font = ('Aerial bold',40), width=26, height=1)
+search_label.place(x=0,y=0)
+search_label.place_forget()
 
-txt = Label(root, text = "this is linear search", relief = RAISED)
+sort_label = Label(root, text='Sorts', fg='black', bg='blanched almond', font = ('Aerial bold',40), width=26, height=1)
+sort_label.place(x=0,y=0)
+sort_label.place_forget()
 
-Sort = Button(root, text='Sort', fg='black', bg='#e6e6e6', font=('Times', 20), width=15,)
-Sort.place(x=130,y=350)
+sorted_arr =  Label(root, text= "Sorted Array is: ", fg='black', bg='blanched almond', font = ('Aerial',15))
 
-enter_arr = Label(root, text = "Enter Array",font =60)
-enter_arr.place(x=200,y=200)
+sorted_arr.place_forget()
+
+search = Label(root, text= "Position of the search item is: ", fg='black', bg='blanched almond', font = ('Aerial',15))
+
+search.place_forget()
+#Search options
+
+
+enter_arr = Label(root, text = "Enter Array",font =80)
+
 
 enter_arr.place_forget()
 
-enter_item = Label(root, text = "Enter Search Item",font =60)
-enter_item.place(x=185,y=300)
+enter_item = Label(root, text = "Enter Search Item",font =80)
+
 
 enter_item.place_forget()
 
 
+#Sort options
+
+enter_arr2 = Label(root, text = "Enter Array",font =80)
+
+
+enter_arr2.place_forget()
+
+
+order_selection = Label(root, text = "Select Order",font =80)
+
+
+order_selection.place_forget()
+
+
+
+#Define functions
 
 def click_search():
     global interval
     if interval ==1:
-        txt.place_forget()
+        search.place_forget()
         Sort.place(x=130,y=350)
         enter_arr.place_forget()
         option.place(x=265,y=220)
@@ -71,34 +130,76 @@ def click_search():
         Radiobutton1.place_forget()
         Radiobutton2.place_forget()
         Search.place(x=400,y=350,)
+        search_label.place_forget()
+        Menu.place_forget()
         interval = 0
     elif interval == 0:
-        txt.place(x=350,y=500)
+        search.place(x=200, y=400)
         option.place_forget()
         Sort.place_forget()
         Search.place_forget()
         enter_arr.place(x=200,y=200)
         widget.place(x=400,y=200)
-        widget2.place(x=400,y=300)
-        enter_item.place(x=197,y=300)
+        widget2.place(x=400,y=280)
+        enter_item.place(x=197,y=280)
         Radiobutton1.place(x=200,y=100)
         Radiobutton2.place(x=500,y=100)
+        search_label.place(x=0,y=0)
+        Menu.place(x=20, y=540)
         interval = 1
 
-Menu = Button(root, text = "Menu", command = click_search)
-Menu.place(x=20, y=560)
 
+def click_sort():
+    global interval
+    if interval ==1:
+        sorted_arr.place_forget()
+        Sort.place(x=130,y=350)
+        enter_arr2.place_forget()
+        option.place(x=265,y=220)
+        widget.place_forget()
+        widget2.place_forget()
+        order_selection.place_forget()
+        Radiobutton3.place_forget()
+        Radiobutton4.place_forget()
+        Radiobutton5.place_forget()
+        Search.place(x=400,y=350,)
+        Ascending.place_forget()
+        Descending.place_forget()
+        sort_label.place_forget()
+        Menu2.place_forget()
+        interval = 0
+    elif interval == 0:
+        sorted_arr.place(x=200, y=400)
+        option.place_forget()
+        Sort.place_forget()
+        Search.place_forget()
+        enter_arr2.place(x=200,y=200)
+        widget.place(x=400,y=200)
+        order_selection.place(x=197,y=300)
+        Radiobutton3.place(x=100,y=100)
+        Radiobutton4.place(x=350,y=100)
+        Radiobutton5.place(x=600,y=100)
+        Ascending.place(x=450,y=260)
+        Descending.place(x=450,y=320)
+        Menu2.place(x=20, y=540)
+        sort_label.place(x=0,y=0)
+        interval = 1
 
+#Search or Sort option
 
-
-
-
-        
+Sort = Button(root, text='Sort', fg='black', bg='#e6e6e6', font=('Times', 20), width=15,command = click_sort )
+Sort.place(x=130,y=350)
 
 Search = Button(root, text='Search', fg='black', bg='#e6e6e6', font=('Times', 20),command = click_search, width=15)
 Search.place(x=400,y=350,)
 
+#Different Menu for disparate options
 
+Menu = Button(root, text = "Menu", command = click_search, font =40 )
+Menu.place_forget()
+
+Menu2 = Button(root, text = "Menu", command = click_sort, font =40)
+Menu2.place_forget()
 
 
 
