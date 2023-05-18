c5a306fd2ca1a5548c462d4fa98919222eb77694
Shayan Kabir <shayan.kabir@gmail.com>
2023-05-18 23:25:38 +1000

    Implemented the Selection Sort

--- HSC_Assessment.py
+++ HSC_Assessment.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 global root
 root = Tk() # Creating an instance
 root.title("Search And Sort") # The name of the GUI
@@ -7,21 +8,31 @@ root.configure(bg="#21243B") # Background colour of the GUI
 global interval
 global txt
 interval = 0
+resultText =StringVar
+
+def switch_to_binary():
+    print("hello")
+    SEARCH_linear.place_forget()
+    SEARCH_binary.place(x=200,y=400)
+
+def switch_to_linear():
+    SEARCH_binary.place_forget()
+    SEARCH_linear.place(x=200,y=400)
 
 
-
+    
 
 #Radiobuttons (Search)
 r= IntVar()
 r.set("1")
-Radiobutton1 = Radiobutton(root, text ="Linear", variable = r, value =1, font =20)
+Radiobutton1 = Radiobutton(root, text ="Linear", variable = r, value =int(1), font =20, comman = switch_to_linear)
 Radiobutton1.place(x=100,y=100)
 
 Radiobutton1.place_forget()
 
 
 
-Radiobutton2 = Radiobutton(root, text ="Binary", variable = r, value =2, font =20)
+Radiobutton2 = Radiobutton(root, text ="Binary", variable = r, value =int(2), font =20, command =switch_to_binary)
 Radiobutton2.place(x=600,y=100)
 
 Radiobutton2.place_forget()
@@ -54,7 +65,7 @@ Descending.place_forget()
 
 
 #Entry Widgets
-Enter_arr_sort = Entry(root, font =40, fg="dark blue")
+Enter_arr_sort = Entry(root, font =50, fg="dark blue")
 Enter_arr_sort.place(x=400,y=200)
 
 Enter_arr_sort.place_forget()
@@ -115,7 +126,7 @@ enter_arr2 = Label(root, text = "Enter Array",font =80)
 enter_arr2.place_forget()
 
 
-order_selection = Label(root, text = "Select Order",font =80)
+order_selection = Label(root, text = "Select Order",font =("Arial",30))
 
 
 order_selection.place_forget()
@@ -127,7 +138,8 @@ order_selection.place_forget()
 def click_search():
     global interval
     if interval ==1:
-        SEARCH.place_forget()
+        SEARCH_linear.place_forget()
+        SEARCH_binary.place_forget()
         search.place_forget()
         Sort.place(x=130,y=350)
         enter_arr.place_forget()
@@ -141,9 +153,14 @@ def click_search():
         search_label.place_forget()
         Menu.place_forget()
         result_label.place_forget()
+        Enter_searchitem.delete(0,100)
+        Enter_arr_search.delete(0,100)
+        result_label.config(text="")
         interval = 0
     elif interval == 0:
-        SEARCH.place(x=200, y=400)
+        r.set("1")
+        Enter_searchitem.get()==""
+        SEARCH_linear.place(x=200, y=400)
         search.place(x=200, y=450)
         option.place_forget()
         Sort.place_forget()
@@ -156,13 +173,14 @@ def click_search():
         Radiobutton2.place(x=500,y=100)
         search_label.place(x=0,y=0)
         Menu.place(x=20, y=540)
-        result_label.place(x=300, y=410)
+        result_label.place(x=500, y=450)
         interval = 1
 
 
 def click_sort():
     global interval
     if interval ==1:
+        result_label_sort.place_forget()
         enter_arr.place_forget()
         SORT.place_forget()
         sorted_arr.place_forget()
@@ -181,58 +199,122 @@ def click_sort():
         interval = 0
         result_label.place_forget()
     elif interval == 0:
-        enter_arr.place(x=200,y=200)
+        enter_arr.place(x=100,y=200)
         SORT.place(x=200, y=450)
         sorted_arr.place(x=200, y=400)
         option.place_forget()
         Sort.place_forget()
         Search.place_forget()
         Enter_arr_sort.place(x=400,y=200)
-        order_selection.place(x=197,y=300)
+        order_selection.place(x=100,y=300)
         Radiobutton3.place(x=100,y=100)
         Radiobutton4.place(x=350,y=100)
         Radiobutton5.place(x=600,y=100)
-        Ascending.place(x=450,y=260)
-        Descending.place(x=450,y=320)
+        Ascending.place(x=450,y=280)
+        Descending.place(x=450,y=340)
         Menu2.place(x=20, y=540)
         sort_label.place(x=0,y=0)
         interval = 1
 
 
 def linear_search():
+    if r.get()==1:
+        input_text = Enter_arr_search.get()
+        numbers = input_text.split()  # Split the input text using space as the delimiter
+
+        try:
+    
+            numbers = [int(num) for num in numbers]  # Convert the numbers from strings to integers
+        
+            search_item = int(Enter_searchitem.get())  # Get the search item as an integer
+        
+            index = -1  #if the search item is not found after iterating through the entire array, the value of index remains as -1.
+            for i, num in enumerate(numbers):  #enumerate retrieves both the value and the index whilst iterating the list 
+                if num == search_item:
+                    index = i
+                    break
+        
+            if index != -1:
+                result_label.config(text=f"Search item found at index: {index}")   #f" f strings are used so that different type of data can be used in a sentence
+            else:
+                result_label.config(text="Search item not found")
+        except ValueError:  
+            result_label.config(text="Invalid input")
+
+            
+
+
+
+    
+        
+def binary_search():
+    
     input_text = Enter_arr_search.get()
     numbers = input_text.split()  # Split the input text using space as the delimiter
-
-    try:
     
+    Beg = 0
+    End = len(numbers)
+    Mid = int((Beg + End)/2)
+
+   
+    numbers = [int(num) for num in numbers]  # Convert the numbers from strings to integers
+
+    search_item = int(Enter_searchitem.get())  # Get the search item as an integer
+
+
+    while Beg <= End:
+        Mid = (Beg + End) // 2  # Compute the midpoint
+
+        if numbers[Mid] == search_item:
+            result_label.config(text=f"Search item found at index: {Mid}")
+            return
+        elif numbers[Mid] < search_item:
+            Beg = Mid + 1  # Update the beginning index
+        else:
+            End = Mid - 1  # Update the ending index
+
+    result_label.config(text="Search item not found")  
+   
+        
+
+def selection():
+    input_text = Enter_arr_sort.get()
+    numbers = input_text.split()
+    
+    try:
         numbers = [int(num) for num in numbers]  # Convert the numbers from strings to integers
         
-        search_item = int(Enter_searchitem.get())  # Get the search item as an integer
+        if len(numbers) == 0: # Error checking for if users do not input a value
+            result_label_sort.config(text="No numbers entered")
+            result_label_sort.place(x=400, y=410) 
+            return
         
-        index = -1  #if the search item is not found after iterating through the entire array, the value of index remains as -1.
-        for i, num in enumerate(numbers):  #enumerate retrieves both the value and the index whilst iterating the list 
-            if num == search_item:
-                index = i
-                break
-        
-        if index != -1:
-            result_label.config(text=f"Search item found at index: {index}")   #f" f strings are used so that different type of data can be used in a sentence
-        else:
-            result_label.config(text="Search item not found")
-    except ValueError:
-        result_label.config(text="Invalid input")
+        length = len(numbers)
+        i = 1
+        for i in range(length):
+            min_index = i
+            for j in range(i + 1, length):
+                if numbers[j] < numbers[min_index]:
+                    min_index = j
+            numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+
+        result_label_sort.config(text=f"Sorted Array is: {numbers}")
+        result_label_sort.place(x=400, y=410)
+    except ValueError:  # Error check for non numeric entries 
+        result_label_sort.config(text="Invalid input (non-numeric value entered)")
+        result_label_sort.place(x=400, y=410)
+     
+
+
+result_label_sort = Label(root, bg="#21243B", fg="white", font=10)
 
 
 
-result_label = Label(root,bg="#21243B",fg = "white")
-result_label.place(x=400, y=500)
-result_label.place_forget()
 
- 
 
 #Search or Sort option
 
-Sort = Button(root, text='Sort', fg='black', bg='#e6e6e6', font=('Times', 20), width=15,command = click_sort )
+Sort = Button(root, text='Sort', fg='black', bg='#e6e6e6', font=('Times', 20), width=15,command = click_sort)
 Sort.place(x=130,y=350)
 
 Search = Button(root, text='Search', fg='black', bg='#e6e6e6', font=('Times', 20),command = click_search, width=15)
@@ -248,21 +330,123 @@ Menu2.place_forget()
 
 #Buttons requiring commands 
 
-SEARCH = Button(root, text= "Search ", fg='black', bg='blanched almond', font = ('Aerial',15), command = linear_search)
-SEARCH.place(x=200, y=450)
-SEARCH.place_forget()
+SEARCH_linear = Button(root, text= "Search with linear", fg='black', bg='blanched almond', font = ('Aerial',15), command = linear_search)
+SEARCH_linear.place(x=200, y=450)
+SEARCH_linear.place_forget()
+
+SEARCH_binary = Button(root, text= "Search with binary", fg='black', bg='blanched almond', font = ('Aerial',15), command = binary_search)
+SEARCH_binary.place(x=200,y=450)
+SEARCH_binary.place_forget()
+
+#SEARCH_sort= Button(root, text= "Search (Sort) ", fg='black', bg='blanched almond', font = ('Aerial',15), command = binary_search)
+#SEARCH.place(x=200, y=450)
 
 
-SORT = Button(root, text= "Sort ", fg='black', bg='blanched almond', font = ('Aerial',15))
+SORT = Button(root, text= "Sort with selection ", fg='black', bg='blanched almond', font = ('Aerial',15), command = selection)
 SORT.place(x=200, y=450)
 SORT.place_forget()
 
 
+result_label = Label(root,bg="#21243B",fg = "white", font =10)
+result_label.place(x=500, y=510)
+result_label.place_forget()
 
 
 info = Button(root, text='info',fg='black', bg='blanched almond', font = ('Aerial',20)).place(x=725,y=525)
 
 mainloop()
+
+
+
+
+
+
+
+
+#Selection
+
+
+
+
+#Bubble
+
+
+         
+    
+def bubble():
+        array = [14, 33, 27, 35, 10]
+n = len(array)
+i = 0
+for i in range(n):
+    Swapped = False 
+    for j in range(n - 1): 
+    #Compare the adjacent elements
+        if array[j] > array[j+1]:
+            #Swap them 
+            more = array[j]
+            array[j] = array[j+1]
+            array[j+1] = more
+            Swapped = True
+            #If no number was swapped that means array is sorted now, break the loop 
+            if Swapped == False: 
+                Break 
+print(array)
+
+    
+    
+def selection():
+    input_text = Enter_arr_sort.get()
+    numbers = input_text.split()
+
+    numbers = [int(num) for num in numbers]
+
+    
+    length = len(numbers)
+    i=1
+#def selectionSort(array, n):
+
+    if len(numbers) == 0:
+        print("no input")
+        result_label_sort.config(text="No numbers entered")
+        result_label_sort.place(x=400, y=410) 
+        return
+
+    try:
+        for i in range(length):
+            min_index = int(i)
+            for j in range(i + 1, length):
+                if numbers[j] < numbers[min_index]:
+                    min_index = j
+            (numbers[i], numbers[min_index]) = (numbers[min_index], numbers[i])
+
+    
+        result_label_sort.config(text=f"Sorted Array is: {numbers}")
+        result_label_sort.place(x=400, y=410)  
+
+    except ValueError:
+        print("error")
+        result_label.config(text="Invalid input")
+
+result_label_sort = Label(root, bg="#21243B", fg="white", font=10)
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
